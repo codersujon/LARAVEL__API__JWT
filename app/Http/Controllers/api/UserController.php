a21ce4b97cd0ae28c62acb1e9259bfdc09dafdcf
MD SUJAN AHMED <csesujon155@gmail.com>
2024-01-07 05:15:49 +0600

    User Register API done

--- app/Http/Controllers/api/UserController.php
+++ app/Http/Controllers/api/UserController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
@@ -12,7 +13,31 @@ class UserController extends Controller
     * User Register (POST, formdata)  Open API
     */
     public function register(Request $request){
+        
+        //Validation
+        $request->validate([
+            "name" => "required",
+            "email" => "required|email|unique:users",
+            "phone_number" => "required|unique:users",
+            "password" => "required|confirmed",
+        ]);
 
+        //User Model
+        User::create([
+            "name" => $request->name,
+            "email" => $request->email,
+            "phone_number" => $request->phone_number,
+            "password" => Hash::make($request->password)
+        ]);
+
+        //response
+
+        return response()->json([
+            "status"=> true,
+            "message"=> "User Created Successfully!"
+        ]);
+
+        
     }
 
     /**
